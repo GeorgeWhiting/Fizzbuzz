3cdd5a530e36c50852603331dbf696238fbeb4e6
George Whiting <george_whiting@hotmail.com>
2017-11-14 12:59:11 +0000

    returns fizzbuzz when given a multiple of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
   case
+    when number % 15 == 0
+      'fizzbuzz'
     when number % 3 == 0
       'fizz'
     when number % 5 == 0
@@ -11,4 +13,4 @@ def fizzbuzz(number)
 
 end
 
-puts fizzbuzz(25)
+puts fizzbuzz(30)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed a multiple of 3' do
+  it 'returns "fizz" when passed a multiple of 3 but not a multiple of 5' do
     expect(fizzbuzz(18)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed a multiple of 5' do
+  it 'returns "buzz" when passed a multiple of 5 but not a multiple of 3' do
     expect(fizzbuzz(25)).to eq 'buzz'
   end
-  it 'returns number when passed number that isnt 3' do
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  end
+  it 'returns number when passed number that isnt a multiple of 3 or 5' do
     expect(fizzbuzz(8)).to eq 8
   end
 end
